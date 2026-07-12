1a384de117d22d5cdf00e7f597149c3c05ed1902
ganasonic <ganasonic@hotmail.com>
2026-07-12 20:01:46 +0900

    メニュー修正

--- config/menu.php
+++ config/menu.php
@@ -25,15 +25,18 @@ return [
     //]
     'homemenu1' => [
         'menu01' => ['name' => 'マイシフト', 'url' => '/myshift', 'class' => 'btn_col01', 'sub_name' => 'シフト登録'],
-        'menu02' => ['name' => 'シフト詳細', 'url' => '/detail', 'class' => 'btn_col04', 'sub_name' => 'シフト詳細確認'],
-        'menu03' => ['name' => '食事確認', 'url' => '/meal', 'class' => 'btn_col02', 'sub_name' => '従食寮食確認'],
+        'menu02' => ['name' => 'シフト詳細', 'url' => '/detail', 'class' => 'btn_col01', 'sub_name' => 'シフト詳細確認'],
+        'menu03' => ['name' => '食事確認', 'url' => '/meal', 'class' => 'btn_col01', 'sub_name' => '従食寮食確認'],
         //'menu04' => ['name' => '簡易シフト', 'url' => '/monthly', 'class' => 'btn_col04', 'sub_name' => '月間シフト確認'],
         //'menu05' => ['name' => '集計', 'url' => '/totalling', 'class' => 'btn_col04', 'sub_name' => '登録件数確認'],
         //'menu06' => ['name' => '従食確認', 'url' => '/lunch', 'class' => 'btn_col02', 'sub_name' => '昼食人数確認'],
         //'menu07' => ['name' => '寮食確認', 'url' => '/dormitorymeal', 'class' => 'btn_col02', 'sub_name' => '寮食人数確認'],
         //'menu08' => ['name' => '指定日詳細', 'url' => '/specified', 'class' => 'btn_col04', 'sub_name' => '指定日詳細確認'],
-        'menu09' => ['name' => '宿泊確認', 'url' => '/dormitorystay', 'class' => 'btn_col05', 'sub_name' => '寮宿泊確認'],
-        'menu10' => ['name' => '送迎確認', 'url' => '/pickupbus', 'class' => 'btn_col03', 'sub_name' => '送迎確認/印刷'],
+        'menu09' => ['name' => '宿泊確認', 'url' => '/dormitorystay', 'class' => 'btn_col01', 'sub_name' => '寮宿泊確認'],
+        //'menu10' => ['name' => '送迎確認', 'url' => '/pickupbus', 'class' => 'btn_col03', 'sub_name' => '送迎確認/印刷'],
+        'menu11' => ['name' => '日時管理', 'url' => '/daily-conditions', 'class' => 'btn_col02', 'sub_name' => 'コンディショニング'],
+        'menu12' => ['name' => '目標設定', 'url' => '/goals', 'class' => 'btn_col03', 'sub_name' => '自身の競技計画'],
+        'menu13' => ['name' => 'トレーニング', 'url' => '/training', 'class' => 'btn_col04', 'sub_name' => 'トレーニング計画'],
         'menu99' => ['name' => 'ログアウト', 'url' => '/logout', 'class' => 'btn_col99', 'sub_name' => 'logout'],
     ]
 
