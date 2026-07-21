6383bd7fa51f28ee202bb59dc4847b6913c2c849
ganasonic <ganasonic@hotmail.com>
2026-07-21 20:50:02 +0900

    コーチコメントが表示されない不具合の改修

--- resources/views/nutritions/show.blade.php
+++ resources/views/nutritions/show.blade.php
@@ -10,6 +10,10 @@
                 <div class="card-body">
                     <h3>日付: {{ $nutrition->nutrition_date }}</h3>
                     <p>メモ: {{ $nutrition->daily_memo }}</p>
+                    {{-- 日ごとのコーチコメント --}}
+                    @if(!empty($nutrition->daily_coach_comment))
+                        <p><strong>コーチコメント:</strong> {{ $nutrition->daily_coach_comment }}</p>
+                    @endif
                 </div>
             </div>
 
@@ -30,6 +34,12 @@
                     @if($detail->photo_path)
                     <div class="mt-3">
                         <img src="{{ asset('storage/'.$detail->photo_path) }}" style="max-width: 100%; border-radius: 5px;">
+                    </div>
+                    @endif
+                    {{-- 食事ごとのコーチコメント --}}
+                    @if(!empty($detail->meal_coach_comment))
+                    <div class="mt-2">
+                        <strong>コーチコメント:</strong> {{ $detail->meal_coach_comment }}
                     </div>
                     @endif
                 </div>
